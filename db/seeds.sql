a769518bc1e6fa101b9b9252f78eff776e634ed3
Michrira <Michaelchristianrader@gmail.com>
2023-04-13 17:48:51 -0700

    working on the server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,8 @@
-INSERT INTO
-    deparment (deparment_name)
-VALUES ("Accounting"), ("Finance"), ("Marketing"), ("R&D"),
-INSERT INTO
-    roles (title, salary, departmnet_id)
-VALUES ("employee", 15, 00 001), ("employee", 15, 00 002), ("employee", 15, 00 003), ("employee", 15, 00 004), ("manager", 30, 00 005),
-INSERT INTO
-    employee (
-        first_name,
-        last_name,
-        role_id,
-        manager_id
-    );
+INSERT INTO department (name)
+VALUES ("Accounting"), ("Finance"), ("Marketing"), ("R&D");
 
-VALUES ("Michael", "Rader", 001), ("Michael", "Huang", 002), ("Emma", "Daily", 003), ("Patricia", "Alberto", 004), ("Bryan", "Swarthout", 005),
+INSERT INTO roles (title, salary, department_id)
+VALUES ("employee", 15, 1), ("employee", 15, 2), ("employee", 15, 3), ("employee", 15, 4), ("manager", 30, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Michael", "Rader", 1, NULL), ("Michael", "Huang", 2, NULL), ("Emma", "Daily", 3, NULL), ("Patricia", "Alberto", 4, NULL), ("Bryan", "Swarthout", 5, NULL);
