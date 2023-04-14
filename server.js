0990f9962b4a2cdc9074b7ffdf93d7ed0b9c2b2a
Michrira <Michaelchristianrader@gmail.com>
2023-04-14 10:42:07 -0700

    still working on server.js

--- server.js
+++ server.js
@@ -1,93 +1,80 @@
+// Import dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
 
-// Create a connection to the database
-const db = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        password: "imthebest",
-        database: "employee_db",
-    },
-    console.log("Connected to the employee_db database.")
-);
+// Create connection to database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "imthebest",
+    database: "employee_db",
+});
+
+// Connect to database
+db.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the employee_db database.");
+    // Start the application
+    start();
+});
 
 // Function to start the application
 function start() {
-    // Prompt the user for what they would like to do
+    // Prompt the user to select an action
     inquirer
         .prompt({
             name: "action",
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "View all departments",
-                "View all roles",
                 "View all employees",
-                "Add a department",
-                "Add a role",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee role",
+                "Update employee manager",
+                "Exit",
             ],
         })
         .then((answer) => {
-            // Call the appropriate function based on user choice
+            // Call the appropriate function based on the user's choice
             switch (answer.action) {
-                case "View all departments":
-                    viewDepartments();
-                    break;
-                case "View all roles":
-                    viewRoles();
-                    break;
                 case "View all employees":
                     viewEmployees();
                     break;
-                case "Add a department":
-                    addDepartment();
+                case "View all employees by department":
+                    viewEmployeesByDepartment();
                     break;
-                case "Add a role":
-                    addRole();
+                case "View all employees by manager":
+                    viewEmployeesByManager();
                     break;
-                default:
-                    console.log(`Invalid action: ${answer.action}`);
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "Remove employee":
+                    removeEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployeeRole();
+                    break;
+                case "Update employee manager":
+                    updateEmployeeManager();
+                    break;
+                case "Exit":
+                    db.end();
+                    console.log("Disconnected from the employee_db database.");
                     break;
             }
         });
-}
-
-start();
-// Function to view all departments
-function viewDepartments() {
-    // Query the database for all departments
-    db.query("SELECT * FROM department", function (err, results) {
-        if (err) throw err;
-        // Log the results to the console
-        console.table(results);
-        // Restart the application
-        start();
-    });
-}
-
-// Function to view all roles
-function viewRoles() {
-    // Query the database for all roles
-    db.query(
-        `SELECT role.id, role.title, role.salary, department.name AS department
-      FROM role
-      LEFT JOIN department ON role.department_id = department.id`,
-        function (err, results) {
-            if (err) throw err;
-            // Log the results to the console
-            console.table(results);
-            // Restart the application
-            start();
-        }
-    );
 }
 
 // Function to view all employees
 function viewEmployees() {
     // Query the database for all employees
     db.query(
-        `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+        `SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
       FROM employee
       LEFT JOIN role ON employee.role_id = role.id
       LEFT JOIN department ON role.department_id = department.id
@@ -101,82 +88,69 @@ function viewEmployees() {
         }
     );
 }
-// Function to add a department
-function addDepartment() {
-    // Prompt the user for the name of the new department
+
+function viewEmployeesByDepartment() {
+  // Query the database for all departments
+  db.query(`SELECT * FROM department`, function (err, departments) {
+    if (err) throw err;
+    // Prompt the user to select a department
     inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "department",
-                message: "What is the name of the new department?",
-            },
-        ])
-        .then(function (answer) {
-            // Insert the new department into the database
-            db.query(
-                "INSERT INTO department SET ?",
-                {
-                    name: answer.department,
-                },
-                function (err, results) {
-                    if (err) throw err;
-                    console.log("New department added successfully!");
-                    // Restart the application
-                    start();
-                }
-            );
-        });
+      .prompt({
+        name: "department",
+        type: "list",
+        message: "Which department would you like to view?",
+        choices: departments.map((department) => department.name),
+      })
+      .then((answer) => {
+        // Query the database for all employees in the selected department
+        db.query(
+          `SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+          FROM employee
+          LEFT JOIN role ON employee.role_id = role.id
+          LEFT JOIN department ON role.department_id = department.id
+          LEFT JOIN employee manager ON employee.manager_id = manager.id
+          WHERE department.name = ?`,
+          [answer.department],
+          function (err, results) {
+            if (err) throw err;
+            // Log the results to the console
+            console.table(results);
+            // Restart the application
+            start();
+          }
+        );
+      });
+  });
 }
 
-// Function to add a role
-function addRole() {
-    // Query the database for all departments to use in the inquirer prompt
-    db.query("SELECT * FROM department", function (err, results) {
-        if (err) throw err;
-        // Prompt the user for the details of the new role
-        inquirer
-            .prompt([
-                {
-                    type: "input",
-                    name: "title",
-                    message: "What is the title of the new role?",
-                },
-                {
-                    type: "input",
-                    name: "salary",
-                    message: "What is the salary of the new role?",
-                },
-                {
-                    type: "list",
-                    name: "department",
-                    message: "Which department does the new role belong to?",
-                    choices: function () {
-                        return results.map(function (department) {
-                            return {
-                                name: department.name,
-                                value: department.id,
-                            };
-                        });
-                    },
-                },
-            ])
-            .then(function (answer) {
-                // Insert the new role into the database
-                db.query(
-                    "INSERT INTO role SET ?",
-                    {
-                        title: answer.title,
-                        salary: answer.salary,
-                        department_id: answer.department,
-                    },
-                    function (err, results) {
-                        if (err) throw err;
-                        console.log("New role added successfully!");
-                        // Restart the application
-                        start();
+
+  // Function to view all employees by manager
+  function viewEmployeesByManager() {
+                        // Prompt user to select manager
+                        inquirer
+                            .prompt({
+                                name: "manager",
+                                type: "list",
+                                message: "Which manager would you like to view?",
+                                choices: getManagers(),
+                            })
+                            .then((answer) => {
+                                // Query the database for all employees under the selected manager
+                                db.query(
+                                    `SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+          FROM employee
+          LEFT JOIN role ON employee.role_id = role.id
+          LEFT JOIN department ON role.department_id = department.id
+          LEFT JOIN employee manager ON employee.manager_id = manager.id
+          WHERE CONCAT(manager.first_name, ' ', manager.last_name) = ?`,
+                                    [answer.manager],
+                                    function (err, results) {
+                                        if (err) throw err;
+                                        // Log the results to the console
+                                        console.table(results);
+                                        // Restart the application
+                                        start();
+                                    }
+                                );
+                            });
                     }
-                );
-            });
-    });
-}
